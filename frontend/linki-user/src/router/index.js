3b41eb2e725c92de843eb853ce435c58163d14fb
nia <jhkdev1@gmail.com>
2025-06-06 21:27:06 +0900

    ✨ feature : 캠페인 목록 조회, 캠페인 등록/상세 조회 추가  (#110)
    * ✨ feature : 마이페이지 - 캠페인 목록 조회 기능 추가
    
    - routes.json 이용하여 CampaignListPage db.json 데이터로 화면 그리기 성공
    
    * ✨ feature : 캠페인 상세 페이지 추가
    
    - db.json campaign-detail 데이터로 캠페인 상세 페이지 테스트 완료
    
    * ✨ feature : 캠페인 등록 페이지 추가
    
    - db.json에 캠페인 등록 데이터 들어가는 것 확인 완료

--- frontend/linki-user/src/router/index.js
+++ frontend/linki-user/src/router/index.js
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import ChannelListPage from '@/views/user/advertiser/ChannelListPage.vue'
 import ChannelDetailPage from '@/views/user/advertiser/ChannelDetailPage.vue'
 import HomeView from '@/views/HomeView.vue'
+import CampaignListPage from '@/views/user/advertiser/CampaignListPage.vue'
+import CampaignDetailPage from '@/views/user/advertiser/CampaignDetailPage.vue'
+import CampaignRegisterPage from '@/views/user/advertiser/CampaignRegisterPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -54,7 +57,22 @@ const router = createRouter({
       path: '/proposals/:id',
       name: 'proposal-detail',
       component: () => import('../views/user/influencer/ProposalDetailView.vue')
-    }
+    }, 
+    {
+      path: '/mypage/campaign-list',
+      name: 'campaign-list',
+      component: CampaignListPage
+    },
+    {
+      path: '/mypage/campaign-detail/:id',
+      name: 'campaign-detail',
+      component: CampaignDetailPage
+    },
+    {
+      path: '/mypage/campaign-register',
+      name: 'campaign-register',
+      component: CampaignRegisterPage
+    },
   ]
 })
 
