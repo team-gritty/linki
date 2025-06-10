ad20a39874b27055cdf030b180387895e6529dcd
dlwjdtjq001 <85023317+dlwjdtjq001@users.noreply.github.com>
2025-06-10 13:55:06 +0900

    Feature/eureka registration (#153)
    * 🔧 chore :linki-api 프로젝트명 rename
    
    - linki-api -> integration-service
    
    * 🔧 chore :setting.gradle 명 변경
    
    - settiong.gradle 명 변경
    
    * ✨ feature : eureka-registration
    
    - eureka server 에 client 로 등록
    
    ---------
    
    Co-authored-by: Seonmin(김선민) <seonmink12@naver.com>

--- integration-service/src/main/java/com/Gritty/Linki/IntegrationApplication.java
+++ integration-service/src/main/java/com/Gritty/Linki/IntegrationApplication.java
@@ -3,7 +3,13 @@ package com.Gritty.Linki;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 @SpringBootApplication
+@EnableDiscoveryClient
+
+
+
 public class IntegrationApplication {
 
 	public static void main(String[] args) {
