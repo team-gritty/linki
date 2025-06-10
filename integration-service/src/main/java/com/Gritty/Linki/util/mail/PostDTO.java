4a6cf490623e7a971b267552b9529e104545363c
minhyeokshin <sinminhyeok@gmail.com>
2025-06-10 16:05:52 +0900

    ✨ feature : Mail Sender 기능 구현
    - Mail Sender 기능 구현

---
+++ integration-service/src/main/java/com/Gritty/Linki/util/mail/PostDTO.java
@@ -0,0 +1,21 @@
+package com.Gritty.Linki.util.mail;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class PostDTO {
+    private String title;
+    private String content;
+    private String author;
+    private String template;
+    private LocalDateTime createdAt;
+    private int writeId;
+}
