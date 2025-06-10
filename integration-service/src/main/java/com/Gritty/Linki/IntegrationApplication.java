a768ca9d0b7e8db94f6566314bafd9a01c590391
dlwjdtjq001 <85023317+dlwjdtjq001@users.noreply.github.com>
2025-06-10 12:42:29 +0900

    🔧 chore :linki-api 프로젝트명 rename (#151)
    * 🔧 chore :linki-api 프로젝트명 rename
    
    - linki-api -> integration-service
    
    * 🔧 chore :setting.gradle 명 변경
    
    - settiong.gradle 명 변경

--- integration-service/src/main/java/com/Gritty/Linki/IntegrationApplication.java
+++ integration-service/src/main/java/com/Gritty/Linki/IntegrationApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LinkiApplication {
+public class IntegrationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LinkiApplication.class, args);
+		SpringApplication.run(IntegrationApplication.class, args);
 	}
-
 }
