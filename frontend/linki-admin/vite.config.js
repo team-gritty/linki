4f1e567978a1ae941ba777789a8098235b16a8f9
minhyeokshin <sinminhyeok@gmail.com>
2025-06-11 12:45:11 +0900

    🔧 chore : 프론트 외부 접속 가능 설정 (#181)
    * 🔧 chore : 프론트 외부 접속 가능 설정
    
    - 프론트 외부 접속 가능 설정
    
    * 🔧 chore : 프론트 외부 접속 가능 설정
    
    - 프론트 외부 접속 가능 설정

--- frontend/linki-admin/vite.config.js
+++ frontend/linki-admin/vite.config.js
@@ -15,11 +15,11 @@ export default defineConfig({
   },
   server: {
     port: 3001,
+    host: '0.0.0.0',
     proxy: {
       '/v1': {
         target: 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+        changeOrigin: true
       }
     }
   }
