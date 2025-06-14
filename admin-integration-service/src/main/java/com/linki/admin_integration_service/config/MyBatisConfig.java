7b0ea2ed6e6a33ec77bc5982cfdeb607eca49a66
minhyeokshin <sinminhyeok@gmail.com>
2025-06-14 22:42:19 +0900

    ✨ feature : admin-config 추가 (#234)
    * ✨ feature : MyBatisConfig 추가
    
    - MyBatisConfig 추가
    - MyBatis basePackage 설정
    - MyBatis 스네이크->카멜 자동 매핑
    - xml 파일 위치 설정
    
    * ✨ feature : Cors Config 설정
    
    - 모둔 경로 허용
    - 모든 메서드 허용
    - 모든 헤더 허용
    - 쿠키 사용
    
    * ✨ feature : JWT Interceptor 구현
    
    - JWT Role이 Admin이 아닌 경우 403 반환
    - JWT 에서 Role 추출하는 로직 구현 필요
    
    * ✨ feature : GlobalExceptionHandler
    
     - GlobalExceptionHandler 구현
     - 모든 종류 예외 처리 메서드 추가
     - IllegalArgumentException 처리 메서드 추가
     - NullPointerException 처리 메서드 추가
     - MethodArgumentNotValidException 처리 메서드 추가
     - 일관된 에러 메시지 포맷 적용
    
    * 🐛 fix : MySQL 의존성 추가
    
    - MySQL 의존성 추가

---
+++ admin-integration-service/src/main/java/com/linki/admin_integration_service/config/MyBatisConfig.java
@@ -0,0 +1,34 @@
+package com.linki.admin_integration_service.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.boot.autoconfigure.ConfigurationCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan(basePackages = "com.linki.admin_integration_service")
+public class MyBatisConfig {
+
+    @Bean
+    public ConfigurationCustomizer configurationCustomizer() {
+        return configuration -> configuration.setMapUnderscoreToCamelCase(true);
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+
+        Resource[] res = new PathMatchingResourcePatternResolver()
+                .getResources("classpath:mapper/**/*.xml");
+        sessionFactory.setMapperLocations(res);
+
+        return sessionFactory.getObject();
+    }
+}
