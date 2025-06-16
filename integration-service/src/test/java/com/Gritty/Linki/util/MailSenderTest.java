184ee031ace15d44c29559ad3248a3969c9683c3
seongjun <kimsj0118@gmail.com>
2025-06-16 16:47:00 +0900

    featuer : 회원가입 구현

--- integration-service/src/test/java/com/Gritty/Linki/util/MailSenderTest.java
+++ integration-service/src/test/java/com/Gritty/Linki/util/MailSenderTest.java
@@ -1,23 +1,23 @@
-//package com.Gritty.Linki.util;
-//
-//import com.Gritty.Linki.util.mail.MailService;
-//import com.Gritty.Linki.util.mail.PostDTO;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//public class MailSenderTest {
-//
-//    @Autowired
-//    private MailService mailSender;
-//
-//    @Test
-//    public void testSendMail() {
-//        PostDTO postDTO = new PostDTO();
-//        postDTO.setTitle("Linki Mail Test Title");
-//        postDTO.setTemplate("mail/newPost");
-//
-//        mailSender.sendPostNotification("sinminhyeok@gmail.com",postDTO);
-//    }
-//}
+package com.Gritty.Linki.util;
+
+import com.Gritty.Linki.util.mail.MailService;
+import com.Gritty.Linki.util.mail.PostDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MailSenderTest {
+
+    @Autowired
+    private MailService mailSender;
+
+    @Test
+    public void testSendMail() {
+        PostDTO postDTO = new PostDTO();
+        postDTO.setTitle("Linki Mail Test Title");
+        postDTO.setTemplate("mail/newPost");
+
+        mailSender.sendPostNotification("sinminhyeok@gmail.com",postDTO);
+    }
+}
