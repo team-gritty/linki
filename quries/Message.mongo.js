c776f1bc138898b7d9022e28efdda16ac9888832
kya <dbsdk4211@naver.com>
2025-06-16 12:27:14 +0900

    🐛 fix : 식별자 id가 message id 가 되도록 수정 - 기존 식별자 아이디와 messageId가 별도 필드로 저장되어 혼란을 야기하므로 식별자 아이디가 messageId가 되도록 수정 (mongoDB에는 _id 필드로 저장, java에서는 messageId로 운용)

--- quries/Message.mongo.js
+++ quries/Message.mongo.js
@@ -29,10 +29,9 @@ for (let seq = 0; seq < 5000; seq++) {
     const randomDate = new Date(baseDate.getTime() + offset * 24 * 60 * 60 * 1000); // ms 단위
 
     messages.push({
-        message_id: "MSG" + seq.toString().padStart(4, '0'),
+        _id: "MSG" + seq.toString().padStart(4, '0'),
         chatroom_id: "CHAT" + Math.floor(seq / 5).toString().padStart(4, '0'),
         message_sender_id: "USER" + Math.floor(Math.random() * 1500).toString().padStart(4, '0'),
-
         message_content: "메시지 내용" + seq,
         message_type: ["TEXT", "IMAGE", "FILE"][Math.floor(Math.random() * 3)],
         message_date: randomDate,
