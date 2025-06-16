5b811bc2d58bc7e46613480cad599543392929db
kya <dbsdk4211@naver.com>
2025-06-16 16:46:37 +0900

    ✅ test : chat 엔티티, repo DB 연동 테스트

--- chat-service/src/test/java/com/ssg/chatservice/ChatAndMessageRepositoryTest.java
+++ chat-service/src/test/java/com/ssg/chatservice/ChatAndMessageRepositoryTest.java
@@ -1,7 +1,9 @@
 package com.ssg.chatservice;
-
-import com.ssg.chatservice.Domain.message.repository.MessageRepository;
+import com.ssg.chatservice.domain.chat.enums.ChatStatus;
+import com.ssg.chatservice.domain.chat.repository.ChatRepository;
+import com.ssg.chatservice.domain.message.repository.MessageRepository;
 import com.ssg.chatservice.config.MongoConfig;
+import com.ssg.chatservice.entity.Chat;
 import com.ssg.chatservice.entity.Message;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
@@ -10,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Instant;
 import java.util.Optional;
@@ -21,9 +24,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 @DisplayName("MongoDB, Mysql(JPA) repository test")
 @Rollback(value = false)
+@Transactional
 public class ChatAndMessageRepositoryTest {
     @Autowired
     private MessageRepository messageRepository;
+    @Autowired
+    private ChatRepository chatRepository;
 
     @Test
     @DisplayName("mongoDB message table 연결 테스트")
@@ -43,11 +49,29 @@ public class ChatAndMessageRepositoryTest {
         Message saved = messageRepository.save(testMessage);
         Optional<Message> result = messageRepository.findById(saved.getMessageId());
 
-        //장 및 조회 결과 검증
+        //저장 및 조회 결과 검증
         assertThat(result).isPresent();
         assertThat(result.get().getMessageId()).isEqualTo("test");
         log.info(result.get().toString());
-
     }
+
+    @Test
+    @DisplayName("mysql(Jpa)chat table 연결 테스트")
+    void chatTest(){
+        Chat chat = Chat.builder()
+                .chatDate(Instant.parse("2025-06-16T13:30:00Z"))
+                .chatStatus(ChatStatus.PENDING)
+                .proposalId("test")
+                .build();
+
+        Chat saved = chatRepository.save(chat);
+        Optional<Chat> result = chatRepository.findById(saved.getChatId());
+
+        assertThat(result).isPresent();
+        assertThat(result.get().getChatId()).isEqualTo(chat.getChatId());
+        log.info(result.get().toString());
+    }
+
+
 }
 
