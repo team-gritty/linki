a861732a1d1b3ade69b1c2301690f307f835a72b
kya <dbsdk4211@naver.com>
2025-06-17 17:42:31 +0900

    ✨ feature : 데이터 통신 feignClient 적용 - 의존성 주입 - 테스트 컨트롤러 구현 - 응답객체 구현

---
+++ chat-service/src/main/java/com/ssg/chatservice/client/PartnerApiClient.java
@@ -0,0 +1,12 @@
+    package com.ssg.chatservice.client;
+
+    import org.springframework.cloud.openfeign.FeignClient;
+    import org.springframework.web.bind.annotation.GetMapping;
+    import org.springframework.web.bind.annotation.PathVariable;
+
+    @FeignClient(name="partner-api",url="http://localhost:8080")
+    public interface PartnerApiClient{
+
+        @GetMapping("/v1/integration-service/api/partners/{id}")
+        PartnerInfoResponse getPartnerInfo(@PathVariable("id") String opponentId);
+    }
