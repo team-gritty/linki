89554f5c1ccfc1ae89ff5b6da7f37c5558eef7e2
kya <dbsdk4211@naver.com>
2025-06-17 17:44:16 +0900

    ✨ feature : 응답/요청 객체 생성

---
+++ chat-service/src/main/java/com/ssg/chatservice/domain/chat/dto/respone/ChatDetailResponeDTO.java
@@ -0,0 +1,22 @@
+package com.ssg.chatservice.domain.chat.dto.respone;
+
+import com.ssg.chatservice.domain.chat.enums.ChatStatus;
+import com.ssg.chatservice.domain.chat.enums.NegoStatus;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Builder
+public class ChatDetailResponeDTO {
+    private String chatId;
+    private String partnerId;
+    private String partnerName;
+    private String proposalId;
+    private NegoStatus negoStatus;
+    private ChatStatus chatStatus;
+    private String profileImage;
+    private String channelName;
+
+}
