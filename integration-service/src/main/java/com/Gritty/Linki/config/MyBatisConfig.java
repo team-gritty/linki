9ba39f7edec9a8c0abb860e4bae8eb7b77e9ea65
dlwjdtjq001 <dlwjdtjq001@gmail.com>
2025-06-17 10:59:14 +0900

    ✨ feature : 로그인, 회원가입
    - 로그인 필터 구현 (토큰 사용으로 인한 커스텀)
    - jwt 발급 , 검증 구현
    - 로그인 성공, 실패처리 구현
    
    - jwt 필터에서 security Holder (찰나의 사용)
    - 권한 인가 처리
    - 회원가입 bycript 암호화
    - 회원가입 uuid -> idgenerator 로 pk refactoring

--- integration-service/src/main/java/com/Gritty/Linki/config/MyBatisConfig.java
+++ integration-service/src/main/java/com/Gritty/Linki/config/MyBatisConfig.java
@@ -1,31 +1,31 @@
-package com.Gritty.Linki.config;
-
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-
-import javax.sql.DataSource;
-
-@Configuration
-@MapperScan(basePackages = "com.Gritty.Linki") // 인터페이스 매퍼 경로
-public class MyBatisConfig {
-    @Bean
-    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
-        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
-        sessionFactory.setDataSource(dataSource);
-
-        // resources/mapper 경로에 있는 XML 파일 인식하도록 설정
-        Resource[] resources = new PathMatchingResourcePatternResolver()
-                .getResources("classpath:/mapper/**/*.xml");
-        sessionFactory.setMapperLocations(resources);
-
-        return sessionFactory.getObject();
-
-
-   }
-}
-
+//package com.Gritty.Linki.config;
+//
+//import org.apache.ibatis.session.SqlSessionFactory;
+//import org.mybatis.spring.SqlSessionFactoryBean;
+//import org.mybatis.spring.annotation.MapperScan;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.io.Resource;
+//import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+//
+//import javax.sql.DataSource;
+//
+//@Configuration
+//@MapperScan(basePackages = "com.Gritty.Linki") // 인터페이스 매퍼 경로
+//public class MyBatisConfig {
+//    @Bean
+//    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+//        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+//        sessionFactory.setDataSource(dataSource);
+//
+//        // resources/mapper 경로에 있는 XML 파일 인식하도록 설정
+//        Resource[] resources = new PathMatchingResourcePatternResolver()
+//                .getResources("classpath:/mapper/**/*.xml");
+//        sessionFactory.setMapperLocations(resources);
+//
+//        return sessionFactory.getObject();
+//
+//
+//   }
+//}
+//
--- integration-service/src/main/java/com/Gritty/Linki/domain/oAuth/signUp/repository/AccountRepository.java
+++ integration-service/src/main/java/com/Gritty/Linki/domain/oAuth/signUp/repository/AccountRepository.java
@@ -3,6 +3,7 @@ package com.Gritty.Linki.domain.oAuth.signUp.repository;
 import com.Gritty.Linki.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AccountRepository extends JpaRepository<User, Integer> {
-
+public interface AccountRepository extends JpaRepository<User, String> {
+    Boolean existsByUserLoginId(String userLoginId);
+    User findByUserLoginId(String userLoginId);
 }
