e58431058c79a46e0342273b4290e4190bbaaed8
kya <dbsdk4211@naver.com>
2025-06-17 20:12:46 +0900

    ✨ feature : 에러 코드 추가

--- chat-service/src/main/java/com/ssg/chatservice/domain/chat/enums/ErrorCode.java
+++ chat-service/src/main/java/com/ssg/chatservice/domain/chat/enums/ErrorCode.java
@@ -8,7 +8,12 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-    CHATROOM_ALREADY_EXIST(HttpStatus.CONFLICT,"이미 존재하는 채팅방 입니다.");
+    CHATROOM_ALREADY_EXIST(HttpStatus.CONFLICT,"이미 존재하는 채팅방 입니다."),
+    CHATROOM_NOT_FOUND(HttpStatus.NOT_FOUND, "채팅방이 존재하지 않습니다."),
+    PARTNER_API_FAILED(HttpStatus.BAD_GATEWAY, "파트너 API 호출에 실패했습니다.");
+
+
+
 
     private final HttpStatus status;
     private final String message;
