861ba6985827c0ef73538be8982a0ff841274b39
kya <dbsdk4211@naver.com>
2025-06-17 20:16:53 +0900

    ✨ feature : stomp config 설정 - 엔드포인트 - 메세지 브로커 경로 지정

---
+++ chat-service/src/main/java/com/ssg/chatservice/config/StompConfig.java
@@ -0,0 +1,30 @@
+package com.ssg.chatservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.messaging.StompSubProtocolErrorHandler;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class StompConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        //클라이언트가 구독할 메세지 브로커 경로
+        registry.enableSimpleBroker("/topic", "/queue");
+        //클라이언트가 발행할 메세지 브로커 경로
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        //엔드포인트 경로
+        registry.addEndpoint("/chat")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+        registry.setErrorHandler(new StompSubProtocolErrorHandler());
+    }
+}
