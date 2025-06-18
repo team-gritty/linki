fd78c750d78dd1e75d36f4c589fde5f9245b4ad8
kya <dbsdk4211@naver.com>
2025-06-18 19:39:27 +0900

    🔧 chore : chat-service proxy 경로 추가

--- frontend/linki-user/vite.config.js
+++ frontend/linki-user/vite.config.js
@@ -18,8 +18,12 @@ export default defineConfig({
     host: '0.0.0.0',
     allowedHosts: ['linki.kr', 'www.linki.kr'],
     proxy: {
-      '/v1/api': {
-        target: 'http://localhost:3000',
+      '/v1/chat-service/api/': {
+        target: 'http://localhost:8000',
+        changeOrigin: true
+      },
+      '/v1/chat-service/ws' : {
+        target: 'http://localhost:8000',
         changeOrigin: true
       }
     }
