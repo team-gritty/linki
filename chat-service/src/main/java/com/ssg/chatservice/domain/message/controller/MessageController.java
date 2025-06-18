8e14c10ef1a9250a750387d13394211d1cf75dc3
kya <dbsdk4211@naver.com>
2025-06-18 12:21:58 +0900

    ♻️ refactor : 메세지 전송 시 메세지에 담긴 유저가 아닌 토큰의 유저로 저장하도록 변경

--- chat-service/src/main/java/com/ssg/chatservice/domain/message/controller/MessageController.java
+++ chat-service/src/main/java/com/ssg/chatservice/domain/message/controller/MessageController.java
@@ -9,8 +9,11 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.security.Principal;
+
+@RestController
 @RequiredArgsConstructor
 public class MessageController {
 
@@ -20,8 +23,12 @@ public class MessageController {
 
     //사용자가 보낸 메세지를 해당 채팅방에 전송
     @MessageMapping("/send/message")
-    public void sendToUser(@Payload ChatMessageRequestDTO chatMessageRequestDTO){
+    public void sendToUser(@Payload ChatMessageRequestDTO chatMessageRequestDTO, Principal principal){
+        // JWT에서 파싱된 인증된 유저 ID
+        String senderId = principal.getName();
         ChatMessageDTO chatMessageDTO = modelMapper.map(chatMessageRequestDTO,ChatMessageDTO.class);
+        // 클라이언트가 보낸 senderId는 무시하고 서버에서 설정
+        chatMessageDTO.setSenderId(senderId);
         //사용자가 보낸 메세지를 해당 채팅방에 전송
         messagingTemplate.convertAndSend("/topic/chat/" + chatMessageDTO.getChatId(),chatMessageDTO);
         //사용자가 보낸 메세지를 DB에 저장
