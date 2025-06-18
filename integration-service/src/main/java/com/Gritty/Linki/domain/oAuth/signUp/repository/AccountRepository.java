7b2e2bb792e02d4b1326a95102b21688abd01a6e
seongjun <kimsj0118@gmail.com>
2025-06-19 00:31:59 +0900

    feature : 일반 유저 마이페이지 비밀번호 변경

--- integration-service/src/main/java/com/Gritty/Linki/domain/oAuth/signUp/repository/AccountRepository.java
+++ integration-service/src/main/java/com/Gritty/Linki/domain/oAuth/signUp/repository/AccountRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface AccountRepository extends JpaRepository<User, String> {
     Boolean existsByUserLoginId(String userLoginId);
     User findByUserLoginId(String userLoginId);
+
+
 }
