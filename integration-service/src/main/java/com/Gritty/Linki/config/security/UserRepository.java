e930012285d7be6d2455d21d901cf4a07ddaccb1
kya <dbsdk4211@naver.com>
2025-06-22 06:09:37 +0900

    ✨ feature : 유저기준 조회 목록 jpa쿼리

--- integration-service/src/main/java/com/Gritty/Linki/config/security/UserRepository.java
+++ integration-service/src/main/java/com/Gritty/Linki/config/security/UserRepository.java
@@ -1,10 +1,12 @@
 package com.Gritty.Linki.config.security;
 
+import com.Gritty.Linki.client.chatClient.dto.InterfaceChatInfoDto;
 import com.Gritty.Linki.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
@@ -12,4 +14,30 @@ public interface UserRepository extends JpaRepository<User, String> {
         Optional<User> findByUserLoginId(String userLoginId);
         @Query("SELECT u.userId FROM User u WHERE u.userLoginId = :userLoginId")
         Optional<String> findUserIdByUserLoginId(@Param("userLoginId") String userLoginId);
+
+        //유저 아이디로 인플루언서의 chatInfo 조회
+        @Query(value = """
+           select u.user_id AS userId,
+                   u.user_login_id AS userLoginId,
+                   p.proposal_id AS proposalId
+           from user u
+           join influencer i on u.user_id = i.user_id
+           join proposal p on i.influencer_id = p.influencer_id
+           where u.user_id = :userId
+    """,nativeQuery = true)
+        List<InterfaceChatInfoDto> findInfluencerChatInfoByUserId(@Param("userId") String userId);
+
+        //유저 아이디로 광고주의 chatInfo 조회
+        @Query(value = """
+            select u.user_id AS userId,
+                   u.user_login_id AS userLoginId,
+                   p.proposal_id AS proposalId
+            from user u
+                     join advertiser a on u.user_id = a.user_id
+                     join campaign c on a.advertiser_id = c.advertiser_id
+                     join proposal p on c.campaign_id = p.campaign_id
+            where u.user_id = :userId
+            """,nativeQuery = true)
+        List<InterfaceChatInfoDto> findAdvertiserChatInfoByUserId(@Param("userId") String userId);
+
 }
