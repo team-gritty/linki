e4123f9457d73a4e2b98d05fee71228ac271c75e
kya <dbsdk4211@naver.com>
2025-06-22 03:21:15 +0900

    ✨ feature : chatInfoClient - partnerInfo 구현

--- integration-service/src/main/java/com/Gritty/Linki/client/chatClient/controller/ChatInfoController.java
+++ integration-service/src/main/java/com/Gritty/Linki/client/chatClient/controller/ChatInfoController.java
@@ -1,18 +1,30 @@
 package com.Gritty.Linki.client.chatClient.controller;
 
+import com.Gritty.Linki.client.chatClient.dto.PartnerInfoDto;
 import com.Gritty.Linki.client.chatClient.dto.respone.ChatInfoResponse;
 import com.Gritty.Linki.client.chatClient.dto.respone.PartnerInfoResponse;
+import com.Gritty.Linki.client.chatClient.service.ChatClientService;
+import com.Gritty.Linki.config.security.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/v1/integration-service/api")
-public class DummyChatInfoController {
+public class ChatInfoController {
+    private final ChatClientService chatClientService;
+    private final ModelMapper modelMapper;
+
+    //파트너 정보 조회
     @GetMapping("/partners/{id}")
-    public PartnerInfoResponse getPartnerInfo(@PathVariable String id) {
-        return new PartnerInfoResponse(id,"test","test","test","test","PENDING");
+    public PartnerInfoResponse getPartnerInfo(@AuthenticationPrincipal CustomUserDetails user, @PathVariable String id) {
+        PartnerInfoDto partnerInfoDto = chatClientService.getPartnerByProposal(user,id);
+        return modelMapper.map(partnerInfoDto, PartnerInfoResponse.class);
     }
 
     @GetMapping("/chatInfo/{id}")
