883d9be375cadc23dff06a61cc3512c1bc37e453
kya <dbsdk4211@naver.com>
2025-06-22 06:11:25 +0900

    ✨ feature : 제안서 아이디로 캠페이 조회 jpa

--- integration-service/src/main/java/com/Gritty/Linki/client/chatClient/dto/ChatInfoDto.java
+++ integration-service/src/main/java/com/Gritty/Linki/client/chatClient/dto/ChatInfoDto.java
@@ -1,12 +1,14 @@
 package com.Gritty.Linki.client.chatClient.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class ChatInfoDto {
     private String opponentId;
     private String opponentName;
--- integration-service/src/main/java/com/Gritty/Linki/domain/user/advertiser/campaign/repository/CampaignRepository.java
+++ integration-service/src/main/java/com/Gritty/Linki/domain/user/advertiser/campaign/repository/CampaignRepository.java
@@ -1,6 +1,7 @@
 package com.Gritty.Linki.domain.user.advertiser.campaign.repository;
 
 import com.Gritty.Linki.entity.Campaign;
+import com.Gritty.Linki.entity.Contract;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -42,4 +43,7 @@ public interface CampaignRepository extends JpaRepository<Campaign, String> {
     @Query("SELECT c FROM Campaign c WHERE c.campaignId IN :campaignIds AND c.advertiser.advertiserId = :advertiserId")
     List<Campaign> findByCampaignIdsAndAdvertiserId(@Param("campaignIds") List<String> campaignIds,
             @Param("advertiserId") String advertiserId);
+
+    Optional<Campaign> findByProposalId(String proposalId);
+
 }
