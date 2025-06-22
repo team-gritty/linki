9191a8ad7fc057986148c27968316d2782e73fa1
kya <dbsdk4211@naver.com>
2025-06-22 16:22:21 +0900

    ✅ test : chatClient 컨트롤단 테스트

--- integration-service/src/main/java/com/Gritty/Linki/config/security/UserRepository.java
+++ integration-service/src/main/java/com/Gritty/Linki/config/security/UserRepository.java
@@ -1,12 +1,9 @@
 package com.Gritty.Linki.config.security;
 
-import com.Gritty.Linki.client.chatClient.dto.InterfaceChatInfoDto;
 import com.Gritty.Linki.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
@@ -14,5 +11,4 @@ public interface UserRepository extends JpaRepository<User, String> {
         Optional<User> findByUserLoginId(String userLoginId);
         @Query("SELECT u.userId FROM User u WHERE u.userLoginId = :userLoginId")
         Optional<String> findUserIdByUserLoginId(@Param("userLoginId") String userLoginId);
-
 }
