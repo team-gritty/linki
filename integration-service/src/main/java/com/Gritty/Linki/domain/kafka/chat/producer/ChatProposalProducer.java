f50a91b41436d6bb3e585496a8004b8aefe831c0
kya <dbsdk4211@naver.com>
2025-06-25 13:33:43 +0900

    ✨ feature : 이벤트 객체 생성 메서드 구현

--- integration-service/src/main/java/com/Gritty/Linki/domain/kafka/chat/producer/ChatProposalProducer.java
+++ integration-service/src/main/java/com/Gritty/Linki/domain/kafka/chat/producer/ChatProposalProducer.java
@@ -1,8 +1,11 @@
 package com.Gritty.Linki.domain.kafka.chat.producer;
 
 
+import com.Gritty.Linki.config.security.CustomUserDetails;
 import com.Gritty.Linki.domain.kafka.chat.Event.Event;
+import com.Gritty.Linki.domain.kafka.chat.enums.EventType;
 
 public interface ChatProposalProducer {
-    void sendEvent(Event event);
+    void sendEvent(CustomUserDetails loginUser, EventType eventType, String proposalId);
+    Event getEvent(CustomUserDetails loginUser, EventType eventType,String proposalId);
 }
--- integration-service/src/main/java/com/Gritty/Linki/domain/kafka/chat/producer/ChatProposalProducerImpl.java
+++ integration-service/src/main/java/com/Gritty/Linki/domain/kafka/chat/producer/ChatProposalProducerImpl.java
@@ -1,6 +1,10 @@
 package com.Gritty.Linki.domain.kafka.chat.producer;
 
+import com.Gritty.Linki.config.security.CustomUserDetails;
+import com.Gritty.Linki.config.security.UserRepository;
 import com.Gritty.Linki.domain.kafka.chat.Event.Event;
+import com.Gritty.Linki.domain.kafka.chat.enums.EventType;
+import com.Gritty.Linki.entity.User;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
@@ -12,11 +16,14 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class ChatProposalProducerImpl implements ChatProposalProducer{
+    private final UserRepository userRepository;
     private final KafkaTemplate<String, String> kafkaTemplate;
     private final ObjectMapper mapper;
 
+    //프로듀서의 이벤트 발행
     @Override
-    public void sendEvent(Event event){
+    public void sendEvent(CustomUserDetails loginUser, EventType eventType,String proposalId){
+        Event event = getEvent(loginUser,eventType,proposalId);
         try {
             //객체를 Json 으로 변환
             String message = mapper.writeValueAsString(event);
@@ -26,6 +33,18 @@ public class ChatProposalProducerImpl implements ChatProposalProducer{
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    //이벤트 객체 생성
+    @Override
+    public Event getEvent(CustomUserDetails loginUser, EventType eventType,String proposalId){
+        User user = userRepository.findById(loginUser.getUserId()).orElseThrow(()-> new RuntimeException("존재하지 않는 회원"));
+        return Event.builder()
+                .eventType(eventType)
+                .userName(user.getUserName())
+                .userEmail(user.getUserEmail())
+                .proposalId(proposalId)
+                .build();
     }
 
 }
