c270100969072ed04cd9251f47b84c2061d55de8
kya <dbsdk4211@naver.com>
2025-06-30 16:54:37 +0900

    🐛 fix : mysql 포트 변경

--- quries/tableInit.sql
+++ quries/tableInit.sql
@@ -212,15 +212,6 @@ CREATE TABLE `notice` (
 	`notice_created_at`	DATETIME	NOT NULL,
 	`notice_status`	TINYINT	NOT NULL	DEFAULT 1	COMMENT '1 = 게시 , 0 = 미게시'
 );
-DROP TABLE IF EXISTS chat;
-
-CREATE TABLE `chat` (
-	`chat_id`	varchar(25)	NOT NULL,
-	`chat_date`	datetime	NOT NULL,
-	`chat_status`	Enum('PENDING','ACTIVE','INACTIVE','DELETE')	NOT NULL	COMMENT '대기/활성/비활성/삭제',
-	`proposal_id`	VARCHAR(25)	NOT NULL	COMMENT '제안서 식별 ID',
-    `nego_status` Enum('PENDING','ACCEPTED','REJECTED','PENDING_SIGN','ONGOING','COMPLETED') NOT NULL COMMENT'제안서 대기/수락/거절/계약서 생성 후 서명 대기/서명 완료 후 계약 이행 중 / 광고 이행 및 정산 완료 상태 '
-);
 
 DROP TABLE IF EXISTS advertiser;
 
