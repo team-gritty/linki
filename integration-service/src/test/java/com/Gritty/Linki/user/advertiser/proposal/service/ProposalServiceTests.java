d39fd478ff836917c422e444bb1ebf3fb1b7feff
Nanhee <jhkdev1@gmail.com>
2025-07-02 09:13:37 +0900

    test : 테스트에서 사용하는 데이터 변경 (테이블 데이터 형식 변경으로 인한) (#518)
    * ✨ feature : 프론트 채널 상세 페이지 의 dto 에 맞춘 response 추가
    
    * feature : '리뷰 작성' 메뉴 클릭 하면 완료된 계약 목록이 뜨고, 클릭하여 리뷰 작성 하고, 등록될 수 있도록 함
    
    * feature : 이미 리뷰 작성한 계약은 리뷰 작성 가능한 목록으로 뜨지 않도록 수정함
    
    * test : 테스트에서 사용하는 데이터 변경 (테이블 데이터 형식 변경으로 인한)
    
    * test : 테스트에서 사용하는 데이터 변경 (테이블 데이터 형식 변경으로 인한)

--- integration-service/src/test/java/com/Gritty/Linki/user/advertiser/proposal/service/ProposalServiceTests.java
+++ integration-service/src/test/java/com/Gritty/Linki/user/advertiser/proposal/service/ProposalServiceTests.java
@@ -29,6 +29,7 @@ public class ProposalServiceTests {
 
     /**
      * CustomUserDetails 실제 객체를 생성하는 메소드
+     * 
      * @return
      */
     private CustomUserDetails createMockUserDetails() {
@@ -64,7 +65,7 @@ public class ProposalServiceTests {
     public void getProposalDetail() {
         log.info("제안서 상세 조회 테스트 시작");
 
-        // 
+        //
         CustomUserDetails mockUser = createMockUserDetails();
 
         try {
