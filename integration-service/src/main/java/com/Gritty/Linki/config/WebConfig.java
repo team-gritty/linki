557fc3fba9e34d214a3ecd6355d5917408af5d67
minhyeokshin <sinminhyeok@gmail.com>
2025-07-03 10:28:19 +0900

    🐛 fix : Cors 설정 apiGateWay
    - 구독자 정보가 없을경우 exception 처리하여서버에서 500 반환 확인
    - 없을 경우 exception -> collection.emptyList() 반환하도록 변경

--- integration-service/src/main/java/com/Gritty/Linki/config/WebConfig.java
+++ integration-service/src/main/java/com/Gritty/Linki/config/WebConfig.java
@@ -9,14 +9,14 @@ public class WebConfig implements WebMvcConfigurer {
     /**
      * CORS 설정: 프론트엔드(Vue, React 등)와 연동 위해 전체 허용 (개발 중)
      */
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*") // 추후 'https://linki-client.com' 등으로 제한 가능
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("*") // 추후 'https://linki-client.com' 등으로 제한 가능
+//                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+//                .allowedHeaders("*")
+//                .allowCredentials(true);
+//    }
 
     /**
      * 인터셉터 등록: JWT 토큰 검증 인터셉터 (현재 미적용)
